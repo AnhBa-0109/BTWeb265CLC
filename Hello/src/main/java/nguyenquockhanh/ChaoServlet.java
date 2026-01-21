9f8ddfb24d2ce4c16157bb354f1ce85956c223e0
Khánh Nguyễn <meikahn0109@gmail.com>
2026-01-21 17:14:47 +0700

    test IDE

---
+++ Hello/src/main/java/nguyenquockhanh/ChaoServlet.java
@@ -0,0 +1,26 @@
+package nguyenquockhanh;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/ChaoServlet")
+public class ChaoServlet extends HttpServlet {
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+	throws ServletException, IOException {
+		response.setContentType("text/html");
+		String noiDungHTML = "";
+		noiDungHTML +="<center><h3>TÔI HỌC WEB JAVA<h3><center>";
+		noiDungHTML +="<hr>";
+		noiDungHTML +="65CNTTCLC";
+		
+		PrintWriter boTraLoi = response.getWriter();
+		boTraLoi.print(noiDungHTML);
+	}
+
+}
